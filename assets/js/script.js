97faefab89916218be9d9ab70daf96cdbf7b0249
puturangga21 <puturangga21@gmail.com>
2022-09-17 17:54:23 +0800

    update index, css, and js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,16 @@
 "use strict";
 
-const elemToggleLeFunc = function (elem) {
+/**
+ * element toggle function
+ */
+
+const elemToggleFunc = function (elem) {
   elem.classList.toggle("active");
 };
+
+/**
+ * navbar variables
+ */
 
 const navbar = document.querySelector("[data-navbar]");
 const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
@@ -12,12 +20,15 @@ const navElemArr = [navToggleBtn, overlay];
 
 for (let i = 0; i < navElemArr.length; i++) {
   navElemArr[i].addEventListener("click", function () {
-    elemToggleLeFunc(navbar);
-    elemToggleLeFunc(overlay);
+    elemToggleFunc(navbar);
+    elemToggleFunc(overlay);
   });
 }
 
-// header sticky
+/**
+ * header sticky
+ */
+
 const header = document.querySelector("[data-header]");
 
 let lastScrollPosition = 0;
@@ -28,8 +39,35 @@ window.addEventListener("scroll", function () {
   if (scrollPosition > lastScrollPosition) {
     header.classList.remove("active");
   } else {
-    header.classList.add("acticve");
+    header.classList.add("active");
   }
 
   lastScrollPosition = scrollPosition <= 0 ? 0 : scrollPosition;
+});
+
+/**
+ * form validation
+ */
+
+const input = document.querySelector("[data-input]");
+const submitBtn = document.querySelector("[data-submit]");
+
+input.addEventListener("input", function () {
+  if (input.checkValidity()) {
+    submitBtn.removeAttribute("disabled");
+  } else {
+    submitBtn.setAttribute("disabled", "");
+  }
+});
+
+/**
+ * go top
+ */
+
+const goTopBtn = document.querySelector("[data-go-top]");
+
+window.addEventListener("scroll", function () {
+  window.scrollY >= 200
+    ? goTopBtn.classList.add("active")
+    : goTopBtn.classList.remove("active");
 });
